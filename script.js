27c3f9519da3b0a8cc57d94bcd71685a451e6712
ElijahWilde <42654406+ElijahWilde@users.noreply.github.com>
2020-04-18 22:14:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 
 
-window.addEventListener('load', function() {
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('img');  // $('img')[0]
-            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-            console.log(img.src);
-            const ImageEditor = new FilerobotImageEditor();
-            ImageEditor.open(img.src);
-            img.onload = imageIsLoaded;
-        }
-    });
-  });
-  
-  function imageIsLoaded() { 
-
-    console.log(this.src);
-      
-    ImageEditor.open(this.src);
-
-}
-
 //const ImageEditor = new FilerobotImageEditor();
-
 //ImageEditor.open('Elijah Wilde ID Photo.jpg');
 
 
@@ -31,15 +9,68 @@ document.getElementById("settings").style.display = "none";
 let numElems = 0;
 let currentButton = "";
 
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            addBackgroundImg(URL.createObjectURL(this.files[0]));
+        }
+    });
+});
+
 function delCurrentButton() {
     let elem = document.getElementById(currentButton);
     elem.parentNode.removeChild(elem);
     toggleSettings("No Button Selected");
 }
 
-function changeColor(color){
+function delBackgroundImg(){
+    let elem = document.getElementById(currentButton);
+    elem.classList.add("noBackgroundImg");
+}
+
+function changeBackgroundColor(color){
     let elem = document.getElementById(currentButton);
     elem.style.backgroundColor = "rgba(" + color[0].toString() + "," + color[1].toString() + "," + color[2].toString() + "," + color[3].toString() + ")";
+}
+
+function toggleTextBackground(){
+    let elem = document.getElementById(currentButton+"t");
+    console.log(elem)
+    if (elem.classList.contains('textBackground')){
+        elem.classList.remove('textBackground');
+    } else {
+        elem.classList.add('textBackground')
+    }
+}
+
+function changeTextColor(color){
+    let elem = document.getElementById(currentButton+"t");
+    elem.style.color = "rgba(" + color[0].toString() + "," + color[1].toString() + "," + color[2].toString() + "," + color[3].toString() + ")";
+}
+
+function addBackgroundImg(url){
+    let elem = document.getElementById(currentButton);
+    if (elem.classList.contains("noBackgroundImg")){
+        elem.classList.remove("noBackgroundImg");
+    }
+    elem.style.backgroundImage = 'url("'+ url +'")';
+}
+
+function changeText(){
+    let elem = document.getElementById(currentButton+"t");
+    let text = document.getElementById('textBox').value;
+    console.log(text)
+    elem.innerHTML = text;
+}
+
+function changeTextWeight() {
+    let elem = document.getElementById(currentButton+"t");
+    elem.style.fontWeight = document.getElementById('textWeight').value;
+}
+
+function changeTextSize() {
+    let elem = document.getElementById(currentButton+"t");
+    elem.style.fontSize = document.getElementById('textSize').value.toString() + "%";
 }
 
 function newElem() {
@@ -47,8 +78,14 @@ function newElem() {
     let elem = document.createElement("div");
     elem.setAttribute("id", "e"+numElems);
     elem.setAttribute("class", "myButton");
+
+    let textHolder = document.createElement("div");
+    textHolder.setAttribute("class", "buttonText");
+    textHolder.setAttribute("id", "e"+numElems+"t");
+
+    elem.appendChild(textHolder);
     elem.addEventListener("click", function(){toggleSettings(this.id)});
-    elem.style.backgroundImage = 'url("Elijah Wilde ID Photo.jpg")';
+
     document.getElementById("elemsGoHere").append(elem);
 }
 
@@ -66,8 +103,8 @@ function toggleSettings(callerID) {
 }
 
 //https://github.com/Simonwep/pickr/blob/master/README.md
-const pickr = Pickr.create({
-    el: '.color-picker',
+const pickr1 = Pickr.create({
+    el: '.color-picker-background',
     theme: 'classic',
 
     swatches: [
@@ -108,8 +145,54 @@ const pickr = Pickr.create({
     }
 });
 
-pickr.on('save', (color, instance) => {
-    changeColor(color.toRGBA());
+const pickr2 = Pickr.create({
+    el: '.color-picker-text',
+    theme: 'classic',
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: true
+        }
+    }
+});
+
+pickr1.on('save', (color, instance) => {
+    changeBackgroundColor(color.toRGBA());
     //console.log('save', color, instance);
 })
 
+pickr2.on('save', (color, instance) => {
+    changeTextColor(color.toRGBA());
+    //console.log('save', color, instance);
+})
